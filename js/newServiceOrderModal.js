d74464563f474ce56df96a19fe97d4468ec14600
Gustavo Maia <gustavomaia@outlook.com>
2017-04-23 20:11:25 -0300

    Altera pasta dos js e dos css

---
+++ js/newServiceOrderModal.js
@@ -0,0 +1,9 @@
+function showNewServiceOrderModal() {
+  document.getElementById('newServiceOrderForm').style.display="inline";
+  document.getElementById('overlay').style.display="inline";
+}
+function hideNewServiceOrderModal() {
+  var x = event.keyCode;
+  document.getElementById('newServiceOrderForm').style.display="none";
+  document.getElementById('overlay').style.display="none";
+}
